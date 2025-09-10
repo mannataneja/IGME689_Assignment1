79ceea270de02528942da3ab545868707ea12ad8
mannataneja <tt3092@g.rit.edu>
2025-09-10 14:59:07 -0400

    Longitude Offset Added

--- IGME689_Assignment1/Assets/Scripts/LightingManager.cs
+++ IGME689_Assignment1/Assets/Scripts/LightingManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Esri.ArcGISMapsSDK.Components;
+using Esri.GameEngine.Geometry;
 
 [ExecuteAlways]
 public class LightingManager : MonoBehaviour
@@ -9,6 +11,8 @@ public class LightingManager : MonoBehaviour
     [Header("Use actual time of day or simulated time")]
     [SerializeField] bool useActualTime;
     [SerializeField, Range(0, 24)] private float SimulatedTimeOfDay;
+
+    [SerializeField] ArcGISMapComponent arcGISMap;
 
     void Update()
     {
@@ -39,10 +43,13 @@ public class LightingManager : MonoBehaviour
         RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
         RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
-        if(DirectionalLight != null)
+        double longitude = arcGISMap.OriginPosition.X;
+        if (longitude < 0) longitude += 360f;
+
+        if (DirectionalLight != null)
         {
             DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
-            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) + (float)longitude - 90f, 0f, 0f));
         }
     }
 }
